5354288ff7c7cd373e0d56ce94fcd16fe8fca9c3
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-01-09 13:38:01 +0530

    api

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.batch_pdf_to_epub, name='batch'),
     path('progress/<str:task_id>/', views.progress_status),
     path('cleanup/<str:task_id>/', views.cleanup_task),
+    path("api/receive/", views.receive_ping),
 ]
